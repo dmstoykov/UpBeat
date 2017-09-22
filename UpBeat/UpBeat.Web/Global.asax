3c2e9012cf6caac0e9c9953e70d531747a556d92
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-09-22 15:51:04 +0300

    initial commit

---
+++ UpBeat/UpBeat.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UpBeat.Web.MvcApplication" Language="C#" %>
---
+++ UpBeat/UpBeat.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UpBeat.Web.Startup))]
+namespace UpBeat.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
