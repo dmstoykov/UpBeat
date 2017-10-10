b70c58d629c7828f6cedd74f60a0c569fb7fe6aa
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-10-11 01:29:20 +0300

    small UI changes and library clearup

--- UpBeat/UpBeat.Web/App_Start/BundleConfig.cs
+++ UpBeat/UpBeat.Web/App_Start/BundleConfig.cs
@@ -19,14 +19,6 @@ namespace UpBeat.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
             bundles.Add(new StyleBundle("~/Content/materialize").Include(
                 "~/Content/materialize.css",
                       "~/Content/Custom/styles.css",
